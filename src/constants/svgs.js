cfe5edc7067c941abb7cb8e489228c93bb9a42ca
fikymar <marcela@creativehandles.com>
2022-04-20 15:16:14 +0200

    svg in separate file

--- src/constants/svgs.js
+++ src/constants/svgs.js
@@ -25,3 +25,18 @@ export const Square = (
     </defs>
   </svg>
 )
+
+export const Copy = (
+  <svg width='24' height='28' viewBox='0 0 24 34' xmlns='http://www.w3.org/2000/svg'>
+    <feBlend mode='difference' in='BackgroundImage' in2='SourceGraphic' />
+    <rect className='fill-dovegray  md:fill-alto' width='14' height='16' rx='1' x='5' y='5' fillOpacity='1' />
+    <rect
+      className='fill-dovegray md:fill-alto  stroke-white md:stroke-everglade'
+      width='14'
+      height='16'
+      rx='1'
+      fillOpacity='1'
+      strokeWidth='2'
+    />
+  </svg>
+)
