e12970e5222ab3c3323ab3f242a358fceb8acd46
Arslan Shahab <arslanshahab@gmail.com>
2022-06-13 06:03:51 +0500

    removed unnecessary code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
 import SimpleReactLightbox from 'simple-react-lightbox'
-import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (!window) {
-      return
-    }
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    const vh = window.innerHeight * 0.01
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-  }, [typeof window])
   return (
     <>
       <Toaster
